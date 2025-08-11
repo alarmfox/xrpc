a77656d62e9dea0030ad4f751eca8bce08cdf801
giuseppe <capassog97@gmail.com>
2025-08-11 14:15:18 +0200

    feat: added log_error macro

--- src/rpc_server_unix.c
+++ src/rpc_server_unix.c
@@ -19,14 +19,14 @@
 
 uint64_t op_sum(uint64_t a, uint64_t b) { return a + b; }
 
-#define bail(MSG, ERR)                                                         \
+#define bail(MSG)                                                              \
   {                                                                            \
     char msg[512];                                                             \
-    if (ERR != 0) {                                                            \
+    if (errno != 0) {                                                          \
       snprintf(msg, sizeof(msg), "%s (errno=%d %s)", MSG, errno,               \
                strerror(errno));                                               \
     }                                                                          \
-    log_message(LOG_LV_ERROR, ERR != 0 ? msg : MSG);                           \
+    log_message(LOG_LV_ERROR, errno != 0 ? msg : MSG);                         \
     exit(EXIT_FAILURE);                                                        \
   }
 
@@ -39,6 +39,14 @@ uint64_t op_sum(uint64_t a, uint64_t b) { return a + b; }
         REQ.req_id, REQ.opcode, REQ.op1, REQ.op2, RES.req_id, RES.opcode,      \
         RES.res);                                                              \
     log_message(LOG_LV_DEBUG, msg);                                            \
+  }
+
+#define log_error(MSG)                                                         \
+  {                                                                            \
+    char msg[512];                                                             \
+    snprintf(msg, sizeof(msg), "%s (errno=%d %s)", MSG, errno,                 \
+             strerror(errno));                                                 \
+    log_message(LOG_LV_ERROR, msg);                                            \
   }
 
 int read_all(int fd, void *buf, ssize_t len) {
@@ -68,7 +76,7 @@ void handle_conn(int fd, const struct server *s) {
   };
 
   if (read_all(fd, (void *)&req, sizeof(struct msg_req)) < 0) {
-    perror("error in receiving request");
+    log_error("error in receiving request");
     goto CLEANUP;
   }
 
@@ -81,7 +89,7 @@ void handle_conn(int fd, const struct server *s) {
 
   // TODO: implement a write_all function
   if (write(fd, (const void *)&res, sizeof(struct msg_res)) < 0) {
-    perror("cannot send result");
+    log_error("error in sending request");
   }
 
 CLEANUP:
@@ -102,18 +110,18 @@ int main() {
     log_set_minimum_level(LOG_LV_DEBUG);
 
   if (fd = socket(AF_UNIX, SOCK_STREAM, 0), fd < 0)
-    bail("socket error", errno);
+    bail("socket");
 
   ret = unlink(UNIX_SOCKET_PATH);
   if (ret < 0 && errno != ENOENT)
-    bail("cannot unlink", errno);
+    bail("cannot");
 
   ret = bind(fd, (const struct sockaddr *)&addr, sizeof(struct sockaddr_un));
   if (ret < 0)
-    bail("bind error", errno);
+    bail("bind");
 
   if (ret = listen(fd, BACKLOG), ret < 0)
-    bail("listen error", errno);
+    bail("listen");
 
   sprintf(buf, "listening on %s", UNIX_SOCKET_PATH);
 
@@ -122,13 +130,13 @@ int main() {
   ret = server_register_handler(srv, OP_SUM, op_sum, 0);
 
   if (ret != RPC_SUCCESS) {
-    perror("cannot register handler");
+    log_error("cannot register handler");
     goto CLEANUP;
   }
 
   log_message(LOG_LV_INFO, buf);
   while (client_fd = accept(fd, 0, 0), client_fd > 0) {
-    log_message(DEBUG, "got a new connection");
+    log_message(LOG_LV_DEBUG, "got a new connection");
     handle_conn(client_fd, srv);
   }
 
