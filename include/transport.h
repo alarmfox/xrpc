78e3aaa7a2c636220dde2ca9e7ef8929e4289ee6
giuseppe <capassog97@gmail.com>
2025-08-13 21:42:32 +0200

    feat: added doc in comments

--- include/transport.h
+++ include/transport.h
@@ -3,10 +3,82 @@
 #include "protocol.h"
 
 struct transport;
+
+/**
+ * @brief Initialize a transport instance.
+ *
+ * This function allocates and initializes the transport backend for a given
+ * implementation (TCP, UNIX socket, TLS, etc.). It sets up the listening
+ * socket and prepares the transport for accepting client connections.
+ *
+ * @param[out] t     Pointer to a transport pointer to be allocated and
+ * initialized.
+ * @param[in]  args  Pointer to a transport-specific configuration struct.
+ *                   The type of this struct depends on the transport backend
+ *                   (e.g., struct transport_args for TCP/UNIX).
+ *
+ * @return void
+ */
 void transport_init(struct transport **t, const void *args);
+
+/**
+ * @brief Accept a new client connection if available.
+ *
+ * This function checks for an incoming connection on the listening socket.
+ * If a new client is waiting, it accepts the connection and stores its state
+ * inside the transport object.
+ *
+ * This call may block depending on the transport implementation and socket
+ * mode.
+ *
+ * @param[in,out] t  Pointer to the transport instance.
+ *
+ * @retval  0  A new client was successfully accepted.
+ * @retval -1  No new client available or an error occurred.
+ */
 int transport_poll_client(struct transport *t);
+
+/**
+ * @brief Receive a request from the connected client.
+ *
+ * Reads a complete `struct request` from the currently connected client.
+ * This function blocks until the full request is received or an error occurs.
+ *
+ * @param[in,out] t  Pointer to the transport instance.
+ * @param[out]   r   Pointer to a `struct request` to fill with received data.
+ *                   The data will be unmarshalled to host byte order.
+ *
+ * @retval  0  Request successfully received.
+ * @retval -1  An error occurred (including client disconnection).
+ */
 int transport_recv(struct transport *t, struct request *r);
+
+/**
+ * @brief Send a response to the connected client.
+ *
+ * Writes a complete `struct response` to the currently connected client.
+ * The function will marshal the response into network byte order before
+ * sending.
+ *
+ * @param[in,out] t  Pointer to the transport instance.
+ * @param[in]    r   Pointer to the `struct response` to send.
+ *
+ * @retval  0  Response successfully sent.
+ * @retval -1  An error occurred while sending.
+ */
 int transport_send(struct transport *t, struct response *r);
+
+/**
+ * @brief Free transport resources.
+ *
+ * Closes any active client and listening sockets, releases all allocated
+ * memory, and resets the transport pointer.
+ *
+ * @param[in,out] t  Pointer to the transport instance to free. Will be set to
+ * NULL.
+ *
+ * @return void
+ */
 void transport_free(struct transport *t);
 
 #endif // !__SERVER_H
