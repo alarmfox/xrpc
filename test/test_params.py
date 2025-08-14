60846ed19b666028163e174a05cbe224e429387a
giuseppe <capassog97@gmail.com>
2025-08-14 13:07:29 +0200

    feat: added testing for all transports at once

--- test/test_params.py
+++ test/test_params.py
@@ -6,12 +6,11 @@ import ssl
 
 UNIX_SOCKET_PATH: str = "/tmp/rpc.sock"
 SERVER_ADDRESS: str = "localhost"
-SERVER_PORT: int = 9000
+SERVER_PORT_TCP: int = 9000
+SERVER_PORT_TLS: int = 9001
 MAX_REPETITONS: int = 2
 
 OP_SUM: int = 0
-
-SUPPORTED_TRANSPORTS: [str] = ["tcp", "unix", "tls"]
 
 
 def test(s: socket.socket, reps: int = 100) -> None:
@@ -29,39 +28,49 @@ def test(s: socket.socket, reps: int = 100) -> None:
         assert res[2] == a + b, "sum is not correct"
 
 
-def unix(path: str) -> None:
+def unix() -> None:
     with socket.socket(socket.AF_UNIX, socket.SOCK_STREAM, 0) as s:
         s.connect(UNIX_SOCKET_PATH)
         test(s)
 
 
-def tcp(address: str, port: int) -> None:
-    with socket.create_connection((address, port)) as s:
+def tcp() -> None:
+    with socket.create_connection((SERVER_ADDRESS, SERVER_PORT_TCP)) as s:
         test(s)
 
 
-def tls(address: str, port: int) -> None:
+def tls() -> None:
     context = ssl.create_default_context(cafile="certs/certificate.crt")
     context.check_hostname = False
 
-    with socket.create_connection((address, port)) as sock:
+    with socket.create_connection((SERVER_ADDRESS, SERVER_PORT_TLS)) as sock:
         with context.wrap_socket(sock, server_hostname="localhost") as ssock:
             print(ssock.version)
             test(ssock)
 
 
+def all() -> None:
+    unix()
+    tcp()
+    tls()
+
+
+SUPPORTED_TRANSPORTS: dict[str, callable] = {
+    "tcp": tcp,
+    "unix": unix,
+    "tls": tls,
+    "all": all,
+}
+
 if __name__ == "__main__":
     if len(sys.argv) != 2:
-        print("Usage: <script>", "|".join(SUPPORTED_TRANSPORTS))
+        print("Usage: <script>", "|".join(SUPPORTED_TRANSPORTS.keys()))
         sys.exit(1)
 
-    transport = sys.argv[1]
-    if transport == "unix":
-        unix(UNIX_SOCKET_PATH)
-    elif transport == "tcp":
-        tcp(SERVER_ADDRESS, SERVER_PORT)
-    elif transport == "tls":
-        tls(SERVER_ADDRESS, SERVER_PORT)
-    else:
-        print("unsupported transport:", transport)
+    fn = SUPPORTED_TRANSPORTS.get(sys.argv[1])
+
+    if fn is None:
+        print("unsupported transport:", sys.argv[1])
         sys.exit(1)
+
+    fn()
