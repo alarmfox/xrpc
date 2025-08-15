1395ad39593c4737f8b00ee321bbb67308b17f77
giuseppe <capassog97@gmail.com>
2025-08-15 21:58:00 +0200

    fix: returning an error if a NULL transport is used

--- src/xrpc_server.c
+++ src/xrpc_server.c
@@ -17,6 +17,9 @@ struct xrpc_server {
 int xrpc_server_create(struct xrpc_server **srv, struct transport *t,
                        size_t max_reqs) {
 
+  if (!t)
+    _print_err_and_return("transport is NULL", XRPC_API_INVALID_TRANSPORT);
+
   struct xrpc_server *s = malloc(sizeof(struct xrpc_server));
 
   if (!s)
@@ -25,7 +28,7 @@ int xrpc_server_create(struct xrpc_server **srv, struct transport *t,
   s->t = t;
   s->max_reqs = max_reqs;
 
-  for (int i = 0; i < MAX_HANDLERS; ++i) {
+  for (size_t i = 0; i < MAX_HANDLERS; ++i) {
     s->handlers[i] = NULL;
   }
 
@@ -45,7 +48,6 @@ int xrpc_server_register(struct xrpc_server *srv, const size_t op,
   if (fn && !(flags & XRPC_RF_OVERWRITE))
     _print_err_and_return("handler already registered at op=%lu",
                           XRPC_API_HANDLER_ALREADY_REGISTERED, op);
-  ;
 
   srv->handlers[op] = handler;
   return XRPC_SUCCESS;
@@ -143,7 +145,7 @@ exit:
 
 void xrpc_server_free(struct xrpc_server *srv) {
   if (!srv) return;
-  for (int i = 0; i < MAX_HANDLERS; ++i) {
+  for (size_t i = 0; i < MAX_HANDLERS; ++i) {
     srv->handlers[i] = 0;
   }
   free(srv);
