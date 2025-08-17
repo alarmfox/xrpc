9377b5acc06053a67826cc245e8e8c4f6d822d7a
giuseppe <capassog97@gmail.com>
2025-08-17 16:22:07 +0200

    fix: macro with do {} while (0)

--- include/xrpc/debug.h
+++ include/xrpc/debug.h
@@ -4,21 +4,25 @@
 
 #ifdef DEBUG
 #define XRPC_DEBUG_PRINT(fmt, ...)                                             \
-  fprintf(stderr, "[XRPC-DEBUG] " fmt "\n", ##__VA_ARGS__)
+  do {                                                                         \
+    fprintf(stderr, "[XRPC-DEBUG] " fmt "\n", ##__VA_ARGS__);                  \
+  } while (0);
 #else
 #define XRPC_DEBUG_PRINT(fmt, ...)                                             \
-  {}
+  do {                                                                         \
+  } while (0)
 #endif
 
 #ifdef DEBUG
 #define _print_err_and_return(fmt, retcode, ...)                               \
-  {                                                                            \
+  do {                                                                         \
     fprintf(stderr, "[XRPC-DEBUG] " fmt "\n", ##__VA_ARGS__);                  \
     return retcode;                                                            \
-  }
+  } while (0)
 #else
 #define _print_err_and_return(fmt, ...)                                        \
-  {}
+  do {                                                                         \
+  } while (0)
 #endif
 
 /*
@@ -30,14 +34,16 @@
 #include <errno.h>
 #include <string.h>
 #define _print_syscall_err_and_return(syscall_name, retcode)                   \
-  {                                                                            \
+  do {                                                                         \
     fprintf(stderr, "[XRPC-DEBUG] %s failed: %s (errno=%d)\n", syscall_name,   \
             strerror(errno), errno);                                           \
     return (retcode);                                                          \
-  }
+  } while (0)
 #else
 #define _print_syscall_err_and_return(syscall_name, retcode)                   \
-  { return (retcode); }
+  do {                                                                         \
+    return (retcode);                                                          \
+  } while (0)
 #endif
 
 /*
@@ -47,16 +53,18 @@
 #ifdef DEBUG
 #include "mbedtls/error.h"
 #define _print_mbedtls_err_and_return(func_name, mbedtls_ret, retcode)         \
-  {                                                                            \
+  do {                                                                         \
     char _mbed_err_buf[128];                                                   \
     mbedtls_strerror(mbedtls_ret, _mbed_err_buf, sizeof(_mbed_err_buf));       \
     fprintf(stderr, "[XRPC-DEBUG] %s failed: %s (ret=%d)\n", func_name,        \
             _mbed_err_buf, mbedtls_ret);                                       \
     return (retcode);                                                          \
-  }
+  } while (0)
 #else
 #define _print_mbedtls_err_and_return(func_name, mbedtls_ret, retcode)         \
-  { return (retcode); }
+  do {                                                                         \
+    return (retcode);                                                          \
+  } while (0)
 #endif
 
 #endif // __XRPC_DEBUG_H
