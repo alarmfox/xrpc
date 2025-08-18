a4ea2004ab86c8b727b3bebc118557a1647ae643
giuseppe <capassog97@gmail.com>
2025-08-18 14:16:26 +0200

    removed mbedtls stuff

--- include/xrpc/debug.h
+++ include/xrpc/debug.h
@@ -46,25 +46,4 @@
   } while (0)
 #endif
 
-/*
- * Print mbedtls-specific error and return given error code.
- * Requires: mbedtls_strerror to be available.
- */
-#ifdef DEBUG
-#include "mbedtls/error.h"
-#define XRPC_PRINT_MBEDTLS_ERR_AND_RETURN(func_name, mbedtls_ret, retcode)     \
-  do {                                                                         \
-    char _mbed_err_buf[128];                                                   \
-    mbedtls_strerror(mbedtls_ret, _mbed_err_buf, sizeof(_mbed_err_buf));       \
-    fprintf(stderr, "[XRPC-DEBUG] %s failed: %s (ret=%d)\n", func_name,        \
-            _mbed_err_buf, mbedtls_ret);                                       \
-    return (retcode);                                                          \
-  } while (0)
-#else
-#define XRPC_PRINT_MBEDTLS_ERR_AND_RETURN(func_name, mbedtls_ret, retcode)     \
-  do {                                                                         \
-    return (retcode);                                                          \
-  } while (0)
-#endif
-
 #endif // __XRPC_DEBUG_H
