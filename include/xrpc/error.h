f8f13c589dd7737ffead9597dd0399213c75a27d
giuseppe <capassog97@gmail.com>
2025-08-26 12:33:57 +0200

    feat: added invalid operation error

--- include/xrpc/error.h
+++ include/xrpc/error.h
@@ -14,7 +14,6 @@
  */
 
 #define XRPC_API_ERR_ALLOC -1
-#define XRPC_API_ERR_BAD_OPID -2
 #define XRPC_API_ERR_HANDLER_ALREADY_REGISTERED -3
 #define XRPC_API_ERR_INVALID_ARGS -5
 #define XRPC_API_ERR_INVALID_TRANSPORT -6
@@ -27,6 +26,7 @@
 #define XRPC_PROTO_ERR_INVALID_DTYPE -50
 #define XRPC_PROTO_ERR_SERIALIZATION_INVALID_ARGS -51
 #define XRPC_PROTO_ERR_VERSION_MISMATCH -52
+#define XRPC_PROTO_ERR_INVALID_OP -53
 
 /*
  * Transport related errors. These can be return from connection oriented
